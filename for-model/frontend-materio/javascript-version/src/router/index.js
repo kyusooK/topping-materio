4605833aa041bb1550f201c3a5bf68c1c1d8b248
kyusoo <rbtn110@uengine.org>
2023-11-06 16:52:34 +0900

    router template 처리

--- for-model/frontend-materio/javascript-version/src/router/index.js
+++ for-model/frontend-materio/javascript-version/src/router/index.js
@@ -1,19 +1,27 @@
+forEach: Model
+fileName: index.js
+path: frontend-materio/javascript-version/src/router
+---
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createRouter, createWebHashHistory } from 'vue-router';
 import routes from '~pages'
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
+    {{#boundedContexts}}
+    {{#aggregates}}
     {
-        path: '/companies',
-        name: 'companies',
-        component: () => import('../views/components/ui/CompanyGrid.vue'),
+        path: '/{{nameCamelCase}}',
+        name: '{{nameCamelCase}}',
+        component: () => import('../views/components/ui/{{namePascalCase}}Grid.vue'),
     },
-    {
-      path: '/sales',
-      name: 'salesOrders',
-      component: () => import('../views/components/ui/SalesOrderGrid.vue'),
-    },
+    {{/aggregates}}
+    {{#views}}
+        {{#ifEquals dataProjection "cqrs"}}
+
+            {{/ifEquals}}
+        {{/views}}
+    {{/boundedContexts}}
     ...setupLayouts(routes),
   ],
   scrollBehavior() {
